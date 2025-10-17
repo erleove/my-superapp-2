1c01b2999c0b15112d18143a4b9d40d955def338
Leonid Ermachkov <ermachkovleonid@gmail.com>
2025-10-17 09:46:04 +0300

    fix: base for vite to correct sources paths in built html

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-superapp-2/",
   plugins: [react()],
   server: {
     watch: {
-      usePolling: true
+      usePolling: true,
     },
   },
   build: {
-    outDir: './docs',
-    assetsDir: '',
+    outDir: "./docs",
+    assetsDir: "",
     emptyOutDir: true,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
